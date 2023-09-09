e2b14bfbf85718c81281a696f140ed333adbf22d
lotigara <lotigara@yandex.ru>
2023-09-09 22:04:49 +0300

    Fixed bug

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,9 +2,7 @@ extends Control
 
 
 func _process(delta):
-	var player_speed_field = get_node("LineEdit")
-	# Global.player_nickname = player_nickname_field
-	# Global.player_speed = player_speed_field
+	pass
 
 func _on_button_play_button_up():
 	get_tree().change_scene_to_file("res://scenes/level_0.tscn")
