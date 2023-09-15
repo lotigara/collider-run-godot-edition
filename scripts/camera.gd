55d5ac5ddc17ae3059f8d6e7d29d99440872ebb4
frozway3 <lotigara@yandex.ru>
2023-09-15 10:08:34 +0300

    Cameras in Godot is interesting thing

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -6,7 +6,9 @@ var move_offset = Vector2()
 func _ready():
 	player = get_parent().get_node("RigidBody2D")
 #	move_offset = position - player.position
+	move_offset = Vector2(50, 50)
 
 func _process(delta):
-#	position = player.position + move_offset
-	position = player.position
+	position = player.position + move_offset
+#	position = player.position
+#	pass
