f03bbf5f48b112063bb849d9e9e00f440e8b6032
frozway3 <lotigara@yandex.ru>
2023-09-19 10:35:22 +0300

    Add prefabs

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -5,8 +5,8 @@ var move_offset = Vector2()
 
 func _ready():
 	player = get_parent().get_node("RigidBody2D")
-#	move_offset = position - player.position
-	move_offset = Vector2(50, 50)
+	move_offset = position - player.position
+#	move_offset = Vector2(50, 50)
 
 func _process(delta):
 	position = player.position + move_offset
